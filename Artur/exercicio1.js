89ac9842ccacb47d451d79183cf0a0f67f24e77b
Artur do Gera <artur.souza@liferay.com>
2021-08-19 16:45:30 -0300

    Html e css incompleto

--- Artur/exercicio1.js
+++ Artur/exercicio1.js
@@ -78,4 +78,76 @@ function objeto(name,age,gender,grade,race){
 var num = 2
 console.log(num*2)
 
-   
+// EXERCICIO 3
+
+// Crie uma classe Automovel com as seguintes caracteristicas
+
+class automovel { 
+    constructor(name,marca,model,paisOrigem){
+        this.name=name;
+        this.marca=marca;
+        this.model=model;
+        this.paisOrigem=paisOrigem;    
+    }  
+}
+var carro1 = new automovel('Gol','Wolksvagem','Gol','Brasil')
+var carro2 = new automovel('Ecosport', 'Ford','Ecosport','Brasil')
+var carro3 = new automovel('Uno','Fiat','Uno','Brasil')
+console.log('\n',carro1 ,'\n\n', carro2, '\n\n', carro3)
+
+
+// EXERCICIO 4
+
+// Criar classe automovel com os atributos
+
+class automoveel{
+    constructor(nome,marca,modelo,paisOrigem,tipoCombustivel){
+        this.nome = nome;
+        this.marca= marca;
+        this.modelo=modelo;
+        this.paisOrigem=paisOrigem;
+        this.tipoCombustivel=tipoCombustivel; 
+    }
+    retornar(){
+        console.log(`\n|Nome: ${this.nome} | Marca: ${this.marca} | Modelo: ${this.modelo} | Pais: ${this.paisOrigem} | Combustivel: ${this.tipoCombustivel} |\n`)
+    }}
+var automoveel1  = new automovel('Gol','Volkswagen','flex','Brasil','Gasolina')
+automoveel1.retornar();
+var automoveel2  = new automovel('Ecosport','Ford','flex','Brasil','Etanol')
+automoveel2.retornar();
+
+// Criar classe carro com os atributos 
+
+class Carro{
+    constructor(velocidadeMax,numeroRodas,temStep,ocupantes){
+        this.velocidadeMax = velocidadeMax;
+        this.numeroRodas= numeroRodas;
+        this.temStep= temStep;
+        this.ocupantes=ocupantes;
+    }
+    retornaar(){
+        console.log(`\n|Velocidade.Max: ${this.velocidadeMax} | Numero.Rodas: ${this.numeroRodas} | Step: ${this.temStep} | Ocupantes: ${this.ocupantes}|\n`)
+    }
+}
+var carro1  = new Carro ('150km','4','sim','5')
+carro1.retornaar();
+var carro2  = new Carro('200km','4','nao','5')
+carro2.retornaar();
+
+//Criar classe moto com os atributos
+
+class Moto{
+    constructor(velocidadeMax,numeroRodas,ocupantes){
+        this.velocidadeMax = velocidadeMax;
+        this.numeroRodas= numeroRodas;
+        this.ocupantes=ocupantes;
+    }
+    return(){
+        console.log(`\n|Velocidade.Max: ${this.velocidadeMax} | Numero.Rodas: ${this.numeroRodas} | Ocupantes: ${this.ocupantes}|\n`)
+    }
+}
+var moto1  = new Moto ('100km','2','2')
+moto1.return();
+var moto2  = new Moto('250km','2','2')
+moto2.return();
+
